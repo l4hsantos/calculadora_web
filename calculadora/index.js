97f2b09b27f532ca6916ffb42fc7bf77de615e70
l4hsantos <lss52@discente.ifpe.edu.br>
2025-09-25 11:04:40 -0300

    Update index.js

--- calculadora/index.js
+++ calculadora/index.js
@@ -16,8 +16,19 @@ const resultado = Number(param1) - Number(param2);
 res.send(`Resultado da subtração: ${resultado}`);
 });
 
+app.get('/multiplicacao/:param1/:param2', (req, res) => {
+const { param1, param2 } = req.params;
+const resultado = Number(param1) * Number(param2);
+res.send(`Resultado da multiplicação: ${resultado}`);
+}
+
+
+app.get('/divisao/:param1/:param2', (req, res) => {
+const { param1, param2 } = req.params; 
+const resultado = Number(param1) / Number(param2);
+res.send(`Resultado da divisão: ${resultado}`);
+}
 
 app.listen(port, () => {
 console.log(`Servidor rodando em http://localhost:${port}`);
 });
-/////oiii
